89be265f73dc1a495da9826487a19bf83919e9af
Irina Popa <irina.oana.popa@gmail.com>
2020-07-06 15:23:11 +0200

    changed background to blue

--- git_recap.rb
+++ git_recap.rb
@@ -3,3 +3,4 @@
 # 2. git status - what has changed, what have I added/deleted or modified
 # 3. git add <name of file> --> add this particular file/ git add . --> add all the files that you've been working on
 # 4. git commit -m "make a message of what you've done"
+# 5. git push origin master --> push my master (local default branch) to Github repo (origin)
