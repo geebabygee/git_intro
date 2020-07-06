d7e0bad4e0c227d9a383f411a612238ec86b5c3e
Irina Popa <irina.oana.popa@gmail.com>
2020-07-06 15:18:36 +0200

    added recap

---
+++ git_recap.rb
@@ -0,0 +1,5 @@
+# GIT PROCESS
+# 1. git init - start tracking my work, only once at beginning of project
+# 2. git status - what has changed, what have I added/deleted or modified
+# 3. git add <name of file> --> add this particular file/ git add . --> add all the files that you've been working on
+# 4. git commit -m "make a message of what you've done"
